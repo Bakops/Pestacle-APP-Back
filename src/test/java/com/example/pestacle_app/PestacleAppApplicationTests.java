fb4e6d89367c55c476e6173bc3fe6f7941827c44
Bakou Toure <bakoutoubo.toure@atos.net>
2025-10-27 15:41:20 +0100

    first commit

---
+++ src/test/java/com/example/pestacle_app/PestacleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pestacle_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PestacleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
