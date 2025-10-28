4116e299a8232d18a8efe121aa631dc72d2b0c91
Bakou Toure <bakoutoubo.toure@atos.net>
2025-10-28 09:53:59 +0100

    Branche dev créer

--- src/main/java/com/example/pestacle_app/model/Utilisateur.java
+++ src/main/java/com/example/pestacle_app/model/Utilisateur.java
@@ -1,4 +1,14 @@
 package com.example.pestacle_app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class Utilisateur {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse;
 }
